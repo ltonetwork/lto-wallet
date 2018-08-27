d17d5d3cd12a84d8b2e289263315fc1e1045e7da
Yevhen Horbachov <killabee1985@gmail.com>
2018-08-27 12:34:07 +0200

    fix block page component

--- apps/explorer/src/app/pages/block/block.component.spec.ts
+++ apps/explorer/src/app/pages/block/block.component.spec.ts
@@ -1,16 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  PageContentModule,
+  TransactionsSectionModule,
+  AMOUNT_DIVIDER,
+  KeyvalueListModule
+} from '@explorer/shared';
+import { MatCardModule, MatProgressSpinnerModule } from '@angular/material';
+import { LtoPublicNodeService } from '@legalthings-one/platform';
 
 import { BlockComponent } from './block.component';
 
 describe('BlockComponent', () => {
   let component: BlockComponent;
   let fixture: ComponentFixture<BlockComponent>;
+  let publicNodeMock: Partial<LtoPublicNodeService>;
 
   beforeEach(async(() => {
+    publicNodeMock = {};
+
     TestBed.configureTestingModule({
-      declarations: [ BlockComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        PageContentModule,
+        TransactionsSectionModule,
+        KeyvalueListModule,
+        MatCardModule,
+        MatProgressSpinnerModule
+      ],
+      declarations: [BlockComponent],
+      providers: [
+        {
+          provide: LtoPublicNodeService,
+          useValue: publicNodeMock
+        },
+        {
+          provide: AMOUNT_DIVIDER,
+          useValue: 1000000000
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {

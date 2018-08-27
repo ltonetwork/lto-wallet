e568c7f90ec87c9933d5397973435bf67e5775eb
Yevhen Horbachov <killabee1985@gmail.com>
2018-08-27 10:48:52 +0200

    remove boilerplate code

--- libs/component-kit/src/lib/component-kit.module.ts
+++ libs/component-kit/src/lib/component-kit.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
-import { ComponentKitComponent } from './component-kit.component';
 
 @NgModule({
-  imports: [
-  ],
-  declarations: [ComponentKitComponent],
-  exports: [ComponentKitComponent]
+  imports: []
 })
-export class ComponentKitModule { }
+export class ComponentKitModule {}
--- libs/component-kit/src/public_api.ts
+++ libs/component-kit/src/public_api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of component-kit
  */
 
-export * from './lib/component-kit.service';
-export * from './lib/component-kit.component';
 export * from './lib/component-kit.module';

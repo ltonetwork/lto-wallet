c4e6af0336eab802518f85cbafcedf057caa37eb
Yevhen Horbachov <killabee1985@gmail.com>
2018-08-27 12:31:00 +0200

    fix address component

--- apps/explorer/src/app/pages/address/address.component.spec.ts
+++ apps/explorer/src/app/pages/address/address.component.spec.ts
@@ -1,16 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  PageContentModule,
+  AmountPipeModule,
+  TransactionsSectionModule,
+  AMOUNT_DIVIDER
+} from '@explorer/shared';
+import { MatCardModule, MatProgressSpinnerModule } from '@angular/material';
+import { LtoPublicNodeService } from '@legalthings-one/platform';
 import { AddressComponent } from './address.component';
 
 describe('AddressComponent', () => {
   let component: AddressComponent;
   let fixture: ComponentFixture<AddressComponent>;
+  let publicNodeMock: Partial<LtoPublicNodeService>;
 
   beforeEach(async(() => {
+    publicNodeMock = {};
+
     TestBed.configureTestingModule({
-      declarations: [ AddressComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        PageContentModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        AmountPipeModule,
+        TransactionsSectionModule
+      ],
+      declarations: [AddressComponent],
+      providers: [
+        {
+          provide: LtoPublicNodeService,
+          useValue: publicNodeMock
+        },
+        {
+          provide: AMOUNT_DIVIDER,
+          useValue: 10000000
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- apps/explorer/src/app/shared/index.ts
+++ apps/explorer/src/app/shared/index.ts
@@ -1,2 +1,3 @@
 export * from './shared.module';
 export * from './pipes';
+export * from './components';

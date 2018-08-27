6566a6084f847c663bcd75baf767e1312389e299
Yevhen Horbachov <killabee1985@gmail.com>
2018-08-27 12:42:58 +0200

    fix home component test

--- apps/explorer/src/app/pages/home/home.component.spec.ts
+++ apps/explorer/src/app/pages/home/home.component.spec.ts
@@ -1,16 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PageContentModule, BlocksListModule, SearchBoxModule } from '@explorer/shared';
+import { MatIconModule, MatProgressSpinnerModule } from '@angular/material';
+import { LtoPublicNodeService } from '@legalthings-one/platform';
+import { NotificationService } from '@explorer/core';
+import { of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let publicNodeMock: Partial<LtoPublicNodeService>;
+  let notificationsMock: Partial<NotificationService>;
 
   beforeEach(async(() => {
+    publicNodeMock = {
+      lastBlocks: () => of([])
+    };
+    notificationsMock = {};
+
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        PageContentModule,
+        BlocksListModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        SearchBoxModule
+      ],
+      declarations: [HomeComponent],
+      providers: [
+        {
+          provide: LtoPublicNodeService,
+          useValue: publicNodeMock
+        },
+        {
+          provide: NotificationService,
+          useValue: notificationsMock
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {

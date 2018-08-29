78b3bdb0db6e71b933ebde6b79543b16a8e8420c
Yevhen Horbachov <killabee1985@gmail.com>
2018-08-29 11:49:38 +0200

    fix tests for restore

--- apps/wallet/src/app/pages/auth/restore/restore.component.spec.ts
+++ apps/wallet/src/app/pages/auth/restore/restore.component.spec.ts
@@ -1,16 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+import { AccountManagementService } from '@wallet/core';
 
 import { RestoreComponent } from './restore.component';
 
 describe('RestoreComponent', () => {
   let component: RestoreComponent;
   let fixture: ComponentFixture<RestoreComponent>;
+  let accountManagerMock: Partial<AccountManagementService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RestoreComponent ]
-    })
-    .compileComponents();
+      imports: [
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MatCardModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
+      ],
+      declarations: [RestoreComponent],
+      providers: [
+        {
+          provide: AccountManagementService,
+          useValue: accountManagerMock
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {

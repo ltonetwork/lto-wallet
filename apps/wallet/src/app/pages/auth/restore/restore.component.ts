37b13d403fd04793c31c43d4b21109fd9b22096f
Yevhen Horbachov <killabee1985@gmail.com>
2018-08-29 11:47:36 +0200

    add ability to restore account from seed

--- apps/wallet/src/app/pages/auth/restore/restore.component.ts
+++ apps/wallet/src/app/pages/auth/restore/restore.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AccountManagementService } from '@wallet/core';
 
 @Component({
   selector: 'lto-restore',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restore.component.scss']
 })
 export class RestoreComponent implements OnInit {
+  restoreForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _accountManager: AccountManagementService, private _router: Router) {
+    this.restoreForm = new FormGroup({
+      seed: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 
+  ngOnInit() {}
+
+  async restoreAccount() {
+    const { seed, name, password } = this.restoreForm.value;
+    const wallet = this._accountManager.generateWallet(seed);
+    await this._accountManager.createAccount(name, password, wallet);
+    this._router.navigate(['/']);
+  }
 }

868103f18fcfa02db52ab05849383cb086810532
Yevhen Horbachov <killabee1985@gmail.com>
2018-09-04 14:10:39 +0200

    add transactions page

--- apps/wallet/src/app/pages/private/transactions/transactions.component.ts
+++ apps/wallet/src/app/pages/private/transactions/transactions.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { LtoPublicNodeService } from '@legalthings-one/platform';
+import { shareReplay, map, combineLatest, filter, switchMap } from 'rxjs/operators';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { AccountManagementService } from '@wallet/core';
+import * as moment from 'moment';
+import { Account } from 'lto-api';
 
 @Component({
   selector: 'lto-wallet-transactions',
@@ -6,10 +12,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent implements OnInit {
+  links = [
+    {
+      path: 'all',
+      label: 'All'
+    },
+    {
+      path: 'sent',
+      label: 'Sent'
+    },
+    {
+      path: 'received',
+      label: 'Received'
+    }
+  ];
 
-  constructor() { }
+  transactions$: Observable<any[]>;
+  filter$: BehaviorSubject<string> = new BehaviorSubject('all');
 
-  ngOnInit() {
+  constructor(
+    private _publicNode: LtoPublicNodeService,
+    private _accountManager: AccountManagementService
+  ) {
+    this.transactions$ = of({ address: '3N3CKa6NFVNd3qaBnnDyFmSyTxdkqNGrDYs' }) //_accountManager.wallet$
+      .pipe(
+        filter((wallet): wallet is Account => !!wallet),
+        switchMap(wallet => _publicNode.transactionsOf(wallet.address, 200))
+      )
+      .pipe(
+        map(results => results[0]),
+        map((transactions: any[]) => {
+          // Now we need to group them by date
+          const grouped = transactions.reduce((group, transaction) => {
+            const date = moment(transaction.timestamp).format('MMMM, D, YYYY');
+            const dateGroup = group[date] || [];
+            dateGroup.push(transaction);
+            return {
+              ...group,
+              [date]: dateGroup
+            };
+          }, {});
+
+          // After transaction have been grouped by date we need to ordrer them
+          return Object.keys(grouped)
+            .reduce(
+              (flattened, date) => {
+                return [
+                  ...flattened,
+                  {
+                    date,
+                    transactions: grouped[date]
+                  }
+                ];
+              },
+              [] as any[]
+            )
+            .sort((a, b) => {
+              return moment(a.date, 'MMMM, D, YYYY').isBefore(moment(b.date, 'MMMM, D, YYYY'))
+                ? 1
+                : -1;
+            });
+        }),
+        shareReplay(1)
+      );
   }
 
+  ngOnInit() {}
+
+  setFilter(filter: string) {
+    this.filter$.next(filter);
+  }
 }
--- apps/wallet/src/app/pages/private/transactions/transactions.module.ts
+++ apps/wallet/src/app/pages/private/transactions/transactions.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@wallet/shared';
+import { MatTabsModule, MatTableModule } from '@angular/material';
+import {
+  DataTableCardModule,
+  TransactionTableModule,
+  AmountPipeModule
+} from '@legalthings-one/component-kit';
+
 import { TransactionsComponent } from './transactions.component';
 
 @NgModule({
   imports: [
     SharedModule,
+    MatTabsModule,
+    MatTableModule,
+    DataTableCardModule,
+    TransactionTableModule,
+    AmountPipeModule,
     RouterModule.forChild([
       {
         path: '',

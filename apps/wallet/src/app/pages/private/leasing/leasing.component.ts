e27cbb3857fbc4385ddda82ae8a7f3711115c3c9
Yevhen Horbachov <killabee1985@gmail.com>
2018-09-06 11:25:45 +0200

    add leasing table

--- apps/wallet/src/app/pages/private/leasing/leasing.component.ts
+++ apps/wallet/src/app/pages/private/leasing/leasing.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { shareReplay, map, combineLatest } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Wallet } from '@wallet/core';
+import { TransactionInfoModal } from '../../../shared';
+import * as moment from 'moment';
 
 @Component({
   selector: 'lto-wallet-leasing',
@@ -6,10 +11,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leasing.component.scss']
 })
 export class LeasingComponent implements OnInit {
+  transactions$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private wallet: Wallet, private transactionInfoModal: TransactionInfoModal) {
+    this.transactions$ = wallet.transactions$.pipe(
+      combineLatest(wallet.address$),
+      map(([transactions, address]) => {
+        // We need to calculate ammount
+        // because for mass transactions we can send several parts
+        return transactions.map((transaction: any) => ({
+          ...transaction,
+          sender: transaction.sender === address ? 'You' : transaction.sender,
+          recipient: transaction.recipient === address ? 'You' : transaction.recipient,
+          amount: this.calculateAmount(transaction, address)
+        }));
+      }),
+      map((transactions: any[]) => {
+        // Now we need to group them by date
+        const grouped = transactions.reduce((group, transaction) => {
+          const date = moment(transaction.timestamp).format('MMMM, D, YYYY');
+          const dateGroup = group[date] || [];
+          dateGroup.push(transaction);
+          return {
+            ...group,
+            [date]: dateGroup
+          };
+        }, {});
 
-  ngOnInit() {
+        // After transaction have been grouped by date we need to ordrer them
+        return Object.keys(grouped)
+          .reduce(
+            (flattened, date) => {
+              return [
+                ...flattened,
+                {
+                  date,
+                  transactions: grouped[date]
+                }
+              ];
+            },
+            [] as any[]
+          )
+          .sort((a, b) => {
+            return moment(a.date, 'MMMM, D, YYYY').isBefore(moment(b.date, 'MMMM, D, YYYY'))
+              ? 1
+              : -1;
+          });
+      }),
+      shareReplay(1)
+    );
   }
 
+  ngOnInit() {}
+
+  calculateAmount(transaction: any, address: string): number {
+    if (transaction.type === 11) {
+      // Mass transaction
+      const amount = transaction.transfers.reduce((sum: number, transfer: any) => {
+        return transaction.sender === address || transfer.recipient === address
+          ? transfer.amount + sum
+          : sum;
+      }, 0);
+
+      return amount;
+    }
+
+    return transaction.amount;
+  }
+
+  showDetails(transaction: any) {
+    this.transactionInfoModal.show(transaction);
+  }
 }
--- apps/wallet/src/app/pages/private/leasing/leasing.module.ts
+++ apps/wallet/src/app/pages/private/leasing/leasing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@wallet/shared';
+import { MatTableModule } from '@angular/material';
 import { LeasingComponent } from './leasing.component';
 
 @NgModule({
   imports: [
     SharedModule,
+    MatTableModule,
     RouterModule.forChild([
       {
         path: '',

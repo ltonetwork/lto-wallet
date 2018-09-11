d5a1a7d06fc97db7c66903cc1f9b47059c9a041b
Yevhen Horbachov <killabee1985@gmail.com>
2018-09-11 14:32:34 +0200

    fix dialog button position

--- apps/wallet/src/app/shared/modals/my-wallet-info-modal/my-wallet-info-modal.module.ts
+++ apps/wallet/src/app/shared/modals/my-wallet-info-modal/my-wallet-info-modal.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatButtonModule } from '@angular/material';
 import { MyWalletInfoModalComponent } from './my-wallet-info-modal.component';
 import { MyWalletInfoModal } from './my-wallet-info-modal';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, FlexLayoutModule],
   declarations: [MyWalletInfoModalComponent],
   entryComponents: [MyWalletInfoModalComponent],
   providers: [MyWalletInfoModal]

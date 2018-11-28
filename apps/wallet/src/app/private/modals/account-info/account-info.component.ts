8050f65047af52e1ab9ee8a70bff81573a14725a
Yevhen Horbachov <zpirit@Yevhens-MBP.localdomain>
2018-11-28 11:36:10 +0100

    Add copy button to account info

--- apps/wallet/src/app/private/modals/account-info/account-info.component.ts
+++ apps/wallet/src/app/private/modals/account-info/account-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { Account } from 'lto-api';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'lto-wallet-account-info',
@@ -8,7 +9,20 @@ import { Account } from 'lto-api';
   styleUrls: ['./account-info.component.scss']
 })
 export class AccountInfoComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public ltoWallet: Account) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public ltoWallet: Account, private snackbar: MatSnackBar) {}
 
   ngOnInit() {}
+
+  copyToClipboard() {
+    const input = document.createElement('input');
+    input.style.position = 'absolute';
+    input.style.bottom = '-1000px';
+    document.body.appendChild(input);
+    input.value = this.ltoWallet.address;
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+
+    this.snackbar.open('Address is copied', 'Dismiss', { duration: 3000 });
+  }
 }
--- apps/wallet/src/app/private/modals/account-info/account-info.module.ts
+++ apps/wallet/src/app/private/modals/account-info/account-info.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountInfoComponent } from './account-info.component';
 import { AccountInfoModal } from './account-info.modal';
-import { MatDialogModule, MatButtonModule } from '@angular/material';
+import {
+  MatDialogModule,
+  MatButtonModule,
+  MatIconModule,
+  MatRippleModule,
+  MatTooltipModule,
+  MatSnackBarModule
+} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AccountInfoComponent],
-  imports: [CommonModule, MatDialogModule, MatButtonModule, FlexLayoutModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatRippleModule,
+    MatTooltipModule,
+    MatSnackBarModule
+  ],
   entryComponents: [AccountInfoComponent],
   providers: [AccountInfoModal]
 })

c883d5de85ab9c7254f8e61423d6a9a163cd3bf9
Yevhen Horbachov <zpirit@Yevhens-MBP.localdomain>
2018-11-28 15:50:07 +0100

    Fix withdraw-modal tests

--- apps/wallet/src/app/private/modals/withdraw-modal/withdraw-modal.component.spec.ts
+++ apps/wallet/src/app/private/modals/withdraw-modal/withdraw-modal.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  MatDialogModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MAT_DIALOG_DATA,
+  MatDialogRef
+} from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 import { WithdrawModalComponent } from './withdraw-modal.component';
+import { AMOUNT_DIVIDER } from '../../../tokens';
+import { AmountDividePipe } from '../../../shared/pipes';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('WithdrawModalComponent', () => {
   let component: WithdrawModalComponent;
@@ -8,9 +19,30 @@ describe('WithdrawModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WithdrawModalComponent ]
-    })
-    .compileComponents();
+      imports: [
+        NoopAnimationsModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule
+      ],
+      declarations: [WithdrawModalComponent, AmountDividePipe],
+      providers: [
+        {
+          provide: AMOUNT_DIVIDER,
+          useValue: 1000000
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: 100
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
---
+++ apps/wallet/src/app/shared/pipes/index.ts
@@ -0,0 +1,3 @@
+export * from './amount-divide.pipe';
+export * from './is-you.pipe';
+export * from './type-label.pipe';

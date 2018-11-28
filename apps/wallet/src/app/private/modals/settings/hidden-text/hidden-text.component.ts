637aa5064cefa73fb2482a7e28a29f19f3e5a565
Yevhen Horbachov <zpirit@Yevhens-MBP.localdomain>
2018-11-28 14:28:02 +0100

    fix styling

--- apps/wallet/src/app/private/modals/settings/hidden-text/hidden-text.component.ts
+++ apps/wallet/src/app/private/modals/settings/hidden-text/hidden-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lto-wallet-hidden-text',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hidden-text.component.scss']
 })
 export class HiddenTextComponent implements OnInit {
-  hidden = true;
+  @Input() hidden = true;
 
   constructor() {}
 

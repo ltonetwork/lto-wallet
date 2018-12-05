f1875abf2c3ac7feb89d6d3dc9ee0a91d8b3d51c
Yevhen Horbachov <killabee1985@gmail.com>
2018-12-05 16:17:29 +0100

    add file upload styling

--- apps/wallet/src/app/anchors/components/file-drop/file-drop.module.ts
+++ apps/wallet/src/app/anchors/components/file-drop/file-drop.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileDropComponent } from './file-drop.component';
+import { MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [FileDropComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, FlexLayoutModule],
   exports: [FileDropComponent]
 })
 export class FileDropModule {}

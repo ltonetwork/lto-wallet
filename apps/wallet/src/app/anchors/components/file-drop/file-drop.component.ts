3427240ee83adf4d4a7c28838dfa496b1eb296e1
Yevhen Horbachov <killabee1985@gmail.com>
2018-12-05 16:56:16 +0100

    create file uploading

--- apps/wallet/src/app/anchors/components/file-drop/file-drop.component.ts
+++ apps/wallet/src/app/anchors/components/file-drop/file-drop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadEvent, FileSystemFileEntry } from 'ngx-file-drop';
 
 @Component({
   selector: 'lto-wallet-file-drop',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-drop.component.scss']
 })
 export class FileDropComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  dropped(event: UploadEvent) {
+    const file = event.files[0].fileEntry as FileSystemFileEntry;
+    file.file((file: File) => {
+      this.readFile(file);
+    });
   }
 
+  fileSelected(event: any) {
+    const file = event.target.files[0];
+    this.readFile(file);
+  }
+
+  private readFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      console.log('Done:');
+      console.log(reader.result);
+    };
+    reader.readAsBinaryString(file);
+  }
 }
--- apps/wallet/src/app/anchors/components/file-drop/file-drop.module.ts
+++ apps/wallet/src/app/anchors/components/file-drop/file-drop.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FileDropComponent } from './file-drop.component';
 import { MatIconModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FileDropModule as NgxFileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   declarations: [FileDropComponent],
-  imports: [CommonModule, MatIconModule, FlexLayoutModule],
+  imports: [CommonModule, MatIconModule, FlexLayoutModule, NgxFileDropModule],
   exports: [FileDropComponent]
 })
 export class FileDropModule {}

70e952195d58e437e474234a33b1e57d905b2dd4
Yevhen Horbachov <killabee1985@gmail.com>
2018-12-18 15:45:44 +0100

    remove debugger

--- apps/wallet/src/app/anchors/anchors.component.ts
+++ apps/wallet/src/app/anchors/anchors.component.ts
@@ -27,7 +27,6 @@ export class AnchorsComponent implements OnInit {
   ) {
     this.groupedAcnhors$ = wallet.anchors$.pipe(
       map(anchors => {
-        debugger;
         const withHash = anchors.map(anchorTransaction => {
           // We need to show HASH in table and this HASH should be HEX-HASH
           const hash =

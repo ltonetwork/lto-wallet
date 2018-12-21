215a083d27cb2e7c54bdc8a49c5b17360f96430f
Yevhen Horbachov <killabee1985@gmail.com>
2018-12-21 12:02:14 +0100

    validate transaction form

--- apps/wallet/src/app/modals/make-transaction/make-transaction.component.ts
+++ apps/wallet/src/app/modals/make-transaction/make-transaction.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material';
 import { Observable } from 'rxjs';
 import { WalletService, IBalance } from '../../core';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'lto-wallet-make-transaction',
@@ -10,25 +11,36 @@ import { WalletService, IBalance } from '../../core';
   styleUrls: ['./make-transaction.component.scss']
 })
 export class MakeTransactionComponent implements OnInit {
-  sendForm: FormGroup;
+  sendForm: FormGroup | null = null;
 
   balance$: Observable<IBalance>;
 
   constructor(public dialog: MatDialogRef<any>, private wallet: WalletService) {
     this.balance$ = wallet.balance$;
 
-    this.sendForm = new FormGroup({
-      recipient: new FormControl('', [Validators.required]),
-      amount: new FormControl(0, [Validators.required]),
-      attachment: new FormControl('', []),
-      fee: new FormControl({ value: 0.001, disabled: true }, [Validators.required])
+    this.balance$.pipe(take(1)).subscribe(balance => {
+      const maxValue = balance.available / balance.amountDivider;
+
+      this.sendForm = new FormGroup({
+        recipient: new FormControl('', [Validators.required]),
+        amount: new FormControl(0, [
+          Validators.required,
+          Validators.min(0),
+          Validators.max(maxValue)
+        ]),
+        attachment: new FormControl('', []),
+        fee: new FormControl({ value: 0.001, disabled: true }, [Validators.required])
+      });
     });
   }
 
   ngOnInit() {}
 
   async send() {
-    const { amount, fee, attachment, recipient } = this.sendForm.value;
+    if (!this.sendForm) {
+      return;
+    }
+
     await this.wallet.transfer(this.sendForm.value);
     this.dialog.close();
   }

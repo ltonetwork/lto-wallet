043456978420081023d9ddacf0bd40cc2938ff25
Yevhen Horbachov <killabee1985@gmail.com>
2018-12-21 15:33:15 +0100

    fix dates

--- apps/wallet/src/app/shared/components/transaction-details/transaction-details.component.ts
+++ apps/wallet/src/app/shared/components/transaction-details/transaction-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
-import { EncoderService } from '../../../core';
+import { EncoderService, TransactionTypes } from '../../../core';
 
 interface AnchorData {
   base64: string;
@@ -13,11 +13,18 @@ interface AnchorData {
   styleUrls: ['./transaction-details.component.scss']
 })
 export class TransactionDetailsComponent implements OnInit, OnChanges {
-  @Input() transaction!: any;
+  @Input() transaction!: LTO.Transaction;
   @Output() close = new EventEmitter();
 
   get isMassTransaction(): boolean {
     return this.transaction && this.transaction.type === 11;
+  }
+
+  get isLeasing(): boolean {
+    return (
+      this.transaction.type === TransactionTypes.LEASING ||
+      this.transaction.type === TransactionTypes.CANCEL_LEASING
+    );
   }
 
   selectedAnchorTpe: 'hex' | 'base64' | 'base58' = 'hex';

3616e153b7826d7cffb3da8445bfad2dd957de21
Yevhen Horbachov <killabee1985@gmail.com>
2018-12-21 09:22:45 +0100

    Add password confirmation validation

--- apps/wallet/src/app/shared/components/credentials-form/credentials-form.module.ts
+++ apps/wallet/src/app/shared/components/credentials-form/credentials-form.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CrendetialsFormComponent } from './credentials-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [ReactiveFormsModule, FlexLayoutModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule
+  ],
   declarations: [CrendetialsFormComponent],
   exports: [CrendetialsFormComponent]
 })

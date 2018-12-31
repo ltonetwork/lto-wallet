1eaaeeab85a481f0aa83b28808fa5b531a3da3f0
Yevhen Horbachov <killabee1985@gmail.com>
2018-12-31 16:04:25 +0100

    add wallet mock

--- apps/wallet/src/app/core/services/mocks/index.ts
+++ apps/wallet/src/app/core/services/mocks/index.ts
@@ -1,3 +1,4 @@
 export * from './auth.mock';
 export * from './public-node.mock';
 export * from './bridge.service.mock';
+export * from './wallet.mock';
---
+++ apps/wallet/src/app/core/services/mocks/wallet.mock.ts
@@ -0,0 +1,37 @@
+import { WalletService } from '../wallet.service';
+import { of } from 'rxjs';
+
+export class WalletServiceMock implements WalletService {
+  static provider = {
+    provide: WalletService,
+    useClass: WalletServiceMock
+  };
+
+  balance$ = of({
+    regular: 0,
+    generating: 0,
+    available: 0,
+    effective: 0,
+    amountDivider: 1
+  });
+
+  address$ = of('test_address');
+
+  transactions$ = of([]);
+  leasingTransactions$ = of([]);
+  dataTransactions$ = of([]);
+  transfers$ = of({
+    total: 0,
+    items: []
+  });
+  anchors$ = of({
+    total: 0,
+    items: []
+  });
+
+  async transfer() {}
+  async lease() {}
+  async cancelLease() {}
+  async withdraw() {}
+  async anchor() {}
+}

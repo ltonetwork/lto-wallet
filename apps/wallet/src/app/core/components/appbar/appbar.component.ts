b851f022e734ce50e152409f934abdd74439e861
Yevhen Horbachov <killabee1985@gmail.com>
2019-01-18 12:44:37 +0100

    Add address to appbar (#77)

--- apps/wallet/src/app/core/components/appbar/appbar.component.ts
+++ apps/wallet/src/app/core/components/appbar/appbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 import { AuthService, IUserAccount, Sidenav } from '../../services';
 
 @Component({
@@ -12,7 +13,12 @@ export class AppbarComponent implements OnInit {
   authenticated$: Observable<boolean>;
   userAccount$: Observable<IUserAccount | null>;
 
-  constructor(private _auth: AuthService, private _router: Router, private _sidenav: Sidenav) {
+  constructor(
+    private _auth: AuthService,
+    private _router: Router,
+    private _sidenav: Sidenav,
+    private _snackbar: MatSnackBar
+  ) {
     this.authenticated$ = _auth.authenticated$;
     this.userAccount$ = _auth.account$;
   }
@@ -26,5 +32,18 @@ export class AppbarComponent implements OnInit {
 
   openSidenav() {
     this._sidenav.open();
+  }
+
+  copyAddressToClipboard(address: string) {
+    const input = document.createElement('input');
+    input.style.position = 'absolute';
+    input.style.bottom = '-1000px';
+    document.body.appendChild(input);
+    input.value = address;
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+
+    this._snackbar.open('Address is copied', 'Dismiss', { duration: 3000 });
   }
 }

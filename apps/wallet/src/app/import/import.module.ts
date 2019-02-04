750a0971ab92e0eec84d3e0b742d01dae47b70e8
Yevhen Horbachov <killabee1985@gmail.com>
2019-02-04 12:41:39 +0100

    show addres while importing and trim seed (#79)

--- apps/wallet/src/app/import/import.module.ts
+++ apps/wallet/src/app/import/import.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ImportComponent } from './import.component';
 
 @NgModule({
   declarations: [ImportComponent],
-  imports: [SharedModule, RouterModule.forChild([])]
+  imports: [SharedModule, RouterModule.forChild([]), ReactiveFormsModule]
 })
 export class ImportModule {}

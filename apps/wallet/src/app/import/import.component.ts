b368584d91ca16d449fd43f2c7e4f9783441549b
Yevhen Horbachov <killabee1985@gmail.com>
2019-02-04 12:38:36 +0100

    show addres while importing and trim seed

--- apps/wallet/src/app/import/import.component.ts
+++ apps/wallet/src/app/import/import.component.ts
@@ -3,6 +3,9 @@ import { MatSnackBar } from '@angular/material';
 import { AuthService } from '../core';
 import { Account } from 'lto-api';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, take, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'lto-import',
@@ -16,20 +19,41 @@ export class ImportComponent implements OnInit {
   stepTemplate!: TemplateRef<any>;
   wallet!: Account;
 
-  constructor(private auth: AuthService, private snackbar: MatSnackBar, private router: Router) {}
+  seedControl = new FormControl('');
+
+  walletAddress$: Observable<string>;
+  account$: Observable<Account | null>;
+
+  constructor(private auth: AuthService, private snackbar: MatSnackBar, private router: Router) {
+    this.account$ = this.seedControl.valueChanges.pipe(
+      map((seed: string) => {
+        try {
+          return this.auth.generateWallet(seed.trim());
+        } catch (err) {
+          return null;
+        }
+      }),
+      shareReplay(1)
+    );
+
+    this.walletAddress$ = this.account$.pipe(
+      map(account => {
+        return account ? account.address : '';
+      })
+    );
+  }
 
   ngOnInit() {
     this.stepTemplate = this.step1;
   }
 
-  restoreWallet(seed: string) {
-    try {
-      this.wallet = this.auth.generateWallet(seed);
-      this.stepTemplate = this.step2;
-    } catch (error) {
-      console.error(error);
-      this.notify('Cannot import account');
-    }
+  gotoStep2() {
+    this.account$.pipe(take(1)).subscribe(account => {
+      if (account) {
+        this.wallet = account;
+        this.stepTemplate = this.step2;
+      }
+    });
   }
 
   saveAccount(credentials: { accountName: string; password: string }) {

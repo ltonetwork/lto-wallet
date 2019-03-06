71fb0312801d90290ae3d7ef520bcde5ed1955d0
Sven <sven@legalthings.io>
2019-03-06 11:09:36 +0100

    Change transfer fee and bridge fee (#88)

--- apps/wallet/src/environments/environment.mainnet.ts
+++ apps/wallet/src/environments/environment.mainnet.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   networkByte: 'L',
-  transfer_fee: 25000000,
+  transfer_fee: 100000000,
   anchor_fee: 25000000,
   bridge: true,
   swapPageEnabled: true,

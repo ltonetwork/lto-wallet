1dc886a40debb1bb8716f53823b455d04146dab9
Yevhen Horbachov <killabee1985@gmail.com>
2019-03-19 16:57:40 +0100

    Add withdraw form validation (#91)

--- apps/wallet/src/app/pages/bridge/bridge-swap/steps/withdraw-form/withdraw-form.component.ts
+++ apps/wallet/src/app/pages/bridge/bridge-swap/steps/withdraw-form/withdraw-form.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit, Output, EventEmitter, Inject } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Output, EventEmitter, Inject, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { Observable, combineLatest, ReplaySubject, Subscription } from 'rxjs';
 import { BridgeService, WalletService, etheriumAddressValidator } from '../../../../../core';
 import { DEFAULT_TRANSFER_FEE } from '../../../../../tokens';
-import { map, withLatestFrom } from 'rxjs/operators';
-
-const BRIDGE_MINIMAL_FEE = 2.25;
+import { map, withLatestFrom, take } from 'rxjs/operators';
 
 @Component({
   selector: 'lto-wallet-withdraw-form',
   templateUrl: './withdraw-form.component.html',
   styleUrls: ['./withdraw-form.component.scss']
 })
-export class WithdrawFormComponent implements OnInit {
+export class WithdrawFormComponent implements OnInit, OnDestroy {
   @Output() close = new EventEmitter();
 
   withdrawForm!: FormGroup;
@@ -25,10 +23,14 @@ export class WithdrawFormComponent implements OnInit {
   transfer$: Promise<any> | null = null;
 
   burnRatePct$!: Observable<number>;
-  burnedTokens$!: Observable<number>;
+  burnedTokens$ = new ReplaySubject<number>(1);
   receiving$!: Observable<number>;
 
-  BRIDGE_MINIMAL_FEE = BRIDGE_MINIMAL_FEE;
+  BRIDGE_MINIMAL_FEE = 2.25;
+
+  maxAmount = 0;
+
+  private _subscriptions = new Subscription();
 
   get cannotSend(): boolean {
     return !this.confirmed || !this.captchaResponse;
@@ -44,17 +46,25 @@ export class WithdrawFormComponent implements OnInit {
     this.burnRatePct$ = this._bridge.burnRate$.pipe(map(rate => rate * 100));
 
     this.withdrawForm = new FormGroup({
-      amount: new FormControl(0, [Validators.min(0), Validators.required]),
+      amount: new FormControl(
+        this.BRIDGE_MINIMAL_FEE,
+        [Validators.min(this.BRIDGE_MINIMAL_FEE), Validators.required],
+        this.validateAmount.bind(this)
+      ),
       address: new FormControl('', [Validators.required, etheriumAddressValidator])
     });
 
-    this.burnedTokens$ = this.withdrawForm.valueChanges.pipe(
-      map(value => value.amount),
-      withLatestFrom(this._bridge.burnRate$),
-      map(([amount, burnRate]) => {
-        const burned = amount * burnRate;
-        return burned < BRIDGE_MINIMAL_FEE ? BRIDGE_MINIMAL_FEE : burned;
-      })
+    this._subscriptions.add(
+      this.withdrawForm.valueChanges
+        .pipe(
+          map(value => value.amount),
+          withLatestFrom(this._bridge.burnRate$),
+          map(([amount, burnRate]) => {
+            const burned = amount * burnRate;
+            return burned < this.BRIDGE_MINIMAL_FEE ? this.BRIDGE_MINIMAL_FEE : burned;
+          })
+        )
+        .subscribe(this.burnedTokens$)
     );
 
     this.receiving$ = this.withdrawForm.valueChanges.pipe(
@@ -64,12 +74,19 @@ export class WithdrawFormComponent implements OnInit {
         return amount - burned;
       })
     );
-    /**
-     * amount : 1
-     * Burn rate: 0.03
-     * Burned tokens: amount * burnRate or BRIDGE_MINIMAL_FEE (whatever is higher)
-     * Reciving: amount - burndTokens
-     */
+
+    this._subscriptions.add(
+      combineLatest(this._wallet.balance$, this._wallet.transferFee$).subscribe(
+        ([balance, transferFee]) => {
+          this.maxAmount =
+            (balance.available - transferFee * balance.amountDivider) / balance.amountDivider;
+        }
+      )
+    );
+  }
+
+  ngOnDestroy() {
+    this._subscriptions.unsubscribe();
   }
 
   goToInputStep() {
@@ -109,5 +126,48 @@ export class WithdrawFormComponent implements OnInit {
   isInvalid(controlName: string) {
     const control = this.withdrawForm.controls[controlName];
     return control.dirty && control.invalid;
+  }
+
+  validateAmount(ctrl: AbstractControl) {
+    return combineLatest(this._wallet.balance$, this._wallet.transferFee$).pipe(
+      map(([balance, transferFee]) => {
+        const amount = ctrl.value * balance.amountDivider;
+        const maxAmount = balance.available - transferFee * balance.amountDivider;
+        const invalid = amount > maxAmount;
+        return invalid ? { max: true } : null;
+      }),
+      take(1)
+    );
+  }
+
+  getFormErrors(): string[] {
+    const errors = [];
+    const amountCtrl = this.withdrawForm.controls.amount;
+    const addressCtrl = this.withdrawForm.controls.address;
+    if (amountCtrl.dirty && amountCtrl.errors) {
+      if (amountCtrl.errors.min) {
+        errors.push('Amount cannot be less than ' + this.BRIDGE_MINIMAL_FEE);
+      }
+
+      if (amountCtrl.errors.max) {
+        errors.push('Maxumum amount is ' + this.maxAmount);
+      }
+
+      if (amountCtrl.errors.required) {
+        errors.push('Amount required');
+      }
+    }
+
+    if (addressCtrl.dirty && addressCtrl.errors) {
+      if (addressCtrl.errors.invalidAddress) {
+        errors.push('Invalid address');
+      }
+
+      if (addressCtrl.errors.required) {
+        errors.push('Address required');
+      }
+    }
+
+    return errors;
   }
 }

54c6a3abc43e252b550db653dbf0f815360dc664
Moesjarraf J <moesjarraf@gmail.com>
2019-06-12 16:48:49 +0200

    Add field to specify recipient binance address

--- apps/wallet/src/app/pages/bridge/bridge-swap/steps/deposit-erc/deposit-erc.component.ts
+++ apps/wallet/src/app/pages/bridge/bridge-swap/steps/deposit-erc/deposit-erc.component.ts
@@ -3,7 +3,7 @@ import { BridgeService, etheriumAddressValidator, WalletService } from '../../..
 import { map, switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { SwapType } from '../../swap-type';
-import { AbstractControl, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, ValidatorFn, Validators, FormGroup, FormControl } from '@angular/forms';
 import * as bech32 from 'bech32';
 
 @Component({
@@ -15,8 +15,10 @@ export class DepositErcComponent implements OnInit {
   @Input() swapType!: SwapType;
   @Output() close = new EventEmitter();
 
+  shouldShowCaptcha = false;
   captchaResponse = '';
   address$: Observable<string> | null = null;
+  depositForm!: FormGroup;
 
   get toTokenType(): string {
     switch (this.swapType) {
@@ -60,23 +62,82 @@ export class DepositErcComponent implements OnInit {
     }
   }
 
+  get shouldSpecifyToAddress(): boolean {
+    switch (this.swapType) {
+      case SwapType.ERC20_BINANCE:
+        return true;
+      default:
+        return false;
+    }
+  }
+
   addressPlaceholder!: string;
 
-  constructor(private _bridge: BridgeService, private _wallet: WalletService) {}
+  constructor(private _bridge: BridgeService, private _wallet: WalletService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.addressPlaceholder = this.swapType === SwapType.ERC20_BINANCE ? 'BINANCE' : 'LTO20';
+    const addressValidators: ValidatorFn[] = [Validators.required];
+
+    if (this.swapType === SwapType.ERC20_BINANCE) {
+      addressValidators.push((ctrl: AbstractControl) => {
+        const address = ctrl.value || '';
+        try {
+          const decodeAddress = bech32.decode(address);
+          if (decodeAddress.prefix === 'tbnb' ||
+            decodeAddress.prefix === 'bnb') {
+            return null;
+          }
+
+          return {
+            invalidAddress: true
+          };
+        } catch (err) {
+          return {
+            invalidAddress: true
+          };
+        }
+      });
+    }
+
+    this.depositForm = new FormGroup({
+      address: new FormControl('', addressValidators)
+    });
+  }
 
   resolveCaptcha(response: string) {
     this.captchaResponse = response;
-    const tokenType = this.swapType === SwapType.ERC20_MAIN ||  this.swapType === SwapType.ERC20_BINANCE ? 'LTO20' : 'BINANCE';
+    const tokenType = this.swapType === SwapType.ERC20_MAIN || this.swapType === SwapType.ERC20_BINANCE ? 'LTO20' : 'BINANCE';
     const toTokenType = this.swapType === SwapType.ERC20_BINANCE ? 'BINANCE' : 'LTO';
     if (this.swapType === SwapType.ERC20_BINANCE) {
-      this.address$ = this._bridge.depositTo('tbnb1308kms9e2r6rlcsp9wwgalfrhdpy25hxewh8wz', response, tokenType, toTokenType);
+      this.address$ = this._bridge.depositTo(this.depositForm.value.address, response, tokenType, toTokenType);
     } else {
       this.address$ = this._wallet.address$.pipe(
         switchMap(address => this._bridge.depositTo(address, response, tokenType, toTokenType))
       );
     }
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.depositForm.controls[controlName];
+    return control.dirty && control.invalid;
+  }
+
+  getFormErrors(): string[] {
+    const errors = [];
+    const addressCtrl = this.depositForm.controls.address;
+
+    if (addressCtrl.dirty && addressCtrl.errors) {
+      if (addressCtrl.errors.invalidAddress) {
+        errors.push('Invalid address');
+      }
+
+      if (addressCtrl.errors.required) {
+        errors.push('Address required');
+      }
+    }
+
+    return errors;
   }
 
   closeClick() {

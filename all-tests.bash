f7ba6c25ad45a48e22682738ce599a794bb8e8c0
Arnold Daniels <arnold@jasny.net>
2021-01-18 12:38:31 +0100

    Don't build and trust the legacy explorer in Travis.

--- all-tests.bash
+++ all-tests.bash
@@ -2,7 +2,5 @@ echo -------------------- libs/platform --------------------
 ng test platform --watch=false
 echo -------------------- libs/component-kit --------------------
 ng test component-kit --watch=false
-echo -------------------- apps/explorer --------------------
-ng test explorer --watch=false
 echo -------------------- apps/wallet --------------------
 ng test wallet --watch=false

407e2fa22ffe02a9523d5f5ae9aba689dbef2fc4
arnold <arnold@jasny.net>
2025-04-14 17:35:29 +0200

    Fixed leasing screen

--- src/app/leasing/leasing.module.ts
+++ src/app/leasing/leasing.module.ts
@@ -7,8 +7,17 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { TransactionConfirmationDialog } from '../components/transaction-confirmation-dialog';
 import { TransactionQrDialog } from '../components/transaction-qr-dialog';
-import { StartLeaseModal } from '../modals';
+import { StartLeaseModal, StartLeaseModalModule } from '../modals';
 import { RouterModule, Routes } from '@angular/router';
+import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
+import {
+  ContentSectionComponent,
+  LoadingSpinnerComponent,
+  TransactionDetailsComponent,
+  TransactionsListModule
+} from '@app/shared/components';
+import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 
 const routes: Routes = [
   {
@@ -25,6 +34,17 @@ const routes: Routes = [
     FlexLayoutModule,
     MatSnackBarModule,
     RouterModule.forChild(routes),
+    MatSidenavContainer,
+    TransactionDetailsComponent,
+    MatSidenav,
+    ContentSectionComponent,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    TransactionsListModule,
+    LoadingSpinnerComponent,
+    MatButton,
+    StartLeaseModalModule,
   ],
   providers: [
     TransactionConfirmationDialog,
